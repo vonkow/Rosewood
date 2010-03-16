76d32aebf83fbbf6abda0fe069fdfbb8dadc0611
Caz vonKow <skopsycats@gmail.com>
2010-03-16 14:03:36 +0800

    Improved Evil Eye Game, now with powerups and more shitty graphics

--- evileye/game.js
+++ evileye/game.js
@@ -1,19 +1,34 @@
 var heroX = 0;
 var heroY = 0;
+var heroXTile = 0;
+var heroYTile = 0;
 var eyeCounter = 0;
+var eyesDead = 0;
+var itemCounter = 0;
+var fatima = false;
+var fatimaCountdown = 0;
+var blind = false;
+var blindEyeCountdown = 0;
+var slow = false;
+var slowCountdown = 0;
+var badLuck = false;
+var badLuckCountdown = 0;
+
 var hero = function(name) {
 	this.base = new rw.ent(name, 'hero', 'd1', 'gif', 30, 30);
 	this.heading = 'd';
 	this.update = function() {
 		heroX = this.base.posX1();
 		heroY = this.base.posY1();
+		heroXTile = this.base.tileX;
+		heroYTile = this.base.tileY;
 		if (rw.key('la')) {
 			if (this.base.posX1()>4) {
 				this.base.move(-5,0).heading = 'l';
 			}
 		}
 		if (rw.key('ra')) {
-			if (this.base.posX2()<496) {
+			if (this.base.posX2()<476) {
 				this.base.move(5,0).heading = 'r';
 			}
 		}
@@ -23,7 +38,7 @@ var hero = function(name) {
 			}
 		}
 		if (rw.key('da')) {
-			if (this.base.posY2()<496) {
+			if (this.base.posY2()<476) {
 				this.base.move(0,5).heading = 'd';
 			}
 		}
@@ -45,10 +60,12 @@ var hero = function(name) {
 	}
 	this.hitMap = [[name,0,0,30,30]];
 	this.iGotHit = function(by) {
-		if (by=='eye') {
-			this.base.hide();
-			alert ("You survived "+eyeCounter+" Evil Eyes");
-			return false;
+		if (!fatima) {
+			if (by=='eye') {
+				this.base.hide();
+				alert ("You killed "+eyesDead+" Evil Eyes");
+				return false;
+			}
 		}
 	}
 }
@@ -57,10 +74,19 @@ var eye = function(name, heading) {
 	this.base = new rw.ent(name, 'eye', heading+1, 'gif', 30, 30);
 	this.heading = heading;
 	this.update = function() {
+		if ((this.base.posX1()<0)||(this.base.posX2()>480)||(this.base.posY1()<0)||(this.base.posY2()>480)) {
+			eyesDead++;
+			this.base.hide();
+			return false;
+		}
 		switch (this.heading) {
 			case 'd':
-				if (this.base.posY1()<heroY) {
-					this.base.move(0,5);
+				if ((this.base.posY1()<heroY)||blind) {
+					if (!slow) {
+						this.base.move(0,5);
+					} else {
+						this.base.move(0,2);
+					}
 				} else {
 					if (this.base.posX1()>heroX) {
 						this.heading = 'l';
@@ -72,8 +98,12 @@ var eye = function(name, heading) {
 				}
 				break;
 			case 'l':
-				if (this.base.posX1()>heroX) {
-					this.base.move(-5,0);
+				if ((this.base.posX1()>heroX)||blind) {
+					if (!slow) {
+						this.base.move(-5,0);
+					} else {
+						this.base.move(-2,0);
+					}
 				} else {
 					if (this.base.posY1()>heroY) {
 						this.heading = 'u';
@@ -85,8 +115,12 @@ var eye = function(name, heading) {
 				}
 				break;
 			case 'u':
-				if (this.base.posY1()>heroY) {
-					this.base.move(0,-5);
+				if ((this.base.posY1()>heroY)||blind) {
+					if (!slow) {
+						this.base.move(0,-5);
+					} else {
+						this.base.move(0,-2);
+					}
 				} else {
 					if (this.base.posX1()<heroX) {
 						this.heading = 'r';
@@ -98,8 +132,12 @@ var eye = function(name, heading) {
 				}
 				break;
 			case 'r':
-				if (this.base.posX1()<heroX) {
-					this.base.move(5,0);
+				if ((this.base.posX1()<heroX)||blind) {
+					if (!slow) {
+						this.base.move(5,0);
+					} else {
+						this.base.move(2,0);
+					}
 				} else {
 					if (this.base.posY1()<heroY) {
 						this.heading = 'd';
@@ -115,10 +153,209 @@ var eye = function(name, heading) {
 	this.hitMap = [['eye',10,10,20,20]];
 	this.iGotHit = function(by) {
 		if ((by=='eye')||(by=='hero')) {
+			eyesDead++;
 			this.base.hide();
 			return false;
 		}
 	}
+}
+
+var item = function() {
+	this.base = new rw.ent('item'+itemCounter++, 'items', 'question', 'gif', 30, 30);
+	this.switchCounter = 0;
+	this.lifeCounter = 200;
+	this.counter = Math.round(Math.random()*3);
+	this.update = function() {
+		if (this.lifeCounter>0) {
+			this.lifeCounter--;
+		} else {
+			this.base.hide();
+			return false;
+		}
+		if (this.switchCounter<4) {
+			this.switchCounter++;
+		} else {
+			this.switchCounter = 0;
+			if (this.counter<3) {
+				this.counter++;
+			} else {
+				this.counter = 0;
+			}
+			switch (this.counter) {
+				case 0:
+					this.base.changeSprite('fatima');
+					break;
+				case 1:
+					this.base.changeSprite('blindeye');
+					break;
+				case 2:
+					this.base.changeSprite('slow');
+					break;
+				case 3:
+					this.base.changeSprite('badluck');
+					break;
+			}
+		}
+	}
+	this.hitMap = [['item',0,0,30,30]];
+	this.iGotHit = function(by) {
+		if (by=='hero') {
+			switch (this.counter) {
+				case 0:
+					fatima = true;
+					fatimaCountdown = 100;
+					rw.newEnt(new fatimaNotification())
+						.base.display('fatima',0,0,0);
+					break;
+				case 1:
+					blind = true;
+					blindEyeCountdown = 100;
+					rw.newEnt(new blindEyeNotification())
+						.base.display('blindeye',0,0,0);
+					break;
+				case 2:
+					slow = true;
+					slowCountdown = 100;
+					rw.newEnt(new slowNotification())
+						.base.display('slow',0,0,0);
+					break;
+				case 3:
+					badLuck = true;
+					badLuckCountdown = 50;
+					rw.newEnt(new badLuckNotification())
+						.base.display('badluck',0,0,0);
+					eyeGenerator();
+					eyeGenerator();
+					eyeGenerator();
+					eyeGenerator();
+					eyeGenerator();
+					break;
+			}
+			this.base.hide();
+			return false;
+		}
+	}
+}
+
+var fatimaNotification = function() {
+	this.base = new rw.ent('fatimanotification'+itemCounter, 'notifications', 'fatima', 'gif', 90, 30);
+	this.update = function() {
+		if (!fatima) {
+			this.base.hide();
+			return false;
+		}
+	}
+}
+
+var fatimaTimer = function() {
+	this.base = new rw.rule(true);
+	this.rule = function() {
+		if (fatimaCountdown>0) {
+			fatimaCountdown--;
+		} else {
+			fatima = false;
+		}
+	}
+}
+
+var blindEyeNotification = function() {
+	this.base = new rw.ent('blindeyenotification'+itemCounter, 'notifications', 'blindeye', 'gif', 90, 30);
+	this.update = function() {
+		if (!blind) {
+			this.base.hide();
+			return false;
+		}
+	}
+}
+
+var blindEyeTimer = function() {
+	this.base = new rw.rule(true);
+	this.rule = function() {
+		if (blindEyeCountdown>0) {
+			blindEyeCountdown--;
+		} else {
+			blind = false;
+		}
+	}
+}
+
+var slowNotification = function() {
+	this.base = new rw.ent('slownotification'+itemCounter, 'notifications', 'slow', 'gif', 90, 30);
+	this.update = function () {
+		if (!slow) {
+			this.base.hide();
+			return false;
+		}
+	}
+}
+
+var slowTimer = function() {
+	this.base = new rw.rule(true);
+	this.rule = function() {
+		if (slowCountdown>0) {
+			slowCountdown--;
+		} else {
+			slow = false;
+		}
+	}
+}
+
+var badLuckNotification = function() {
+	this.base = new rw.ent('badlucknotification'+itemCounter, 'notifications', 'badluck', 'gif', 90, 30);
+	this.update = function() {
+		if (!badLuck) {
+			this.base.hide();
+			return false;
+		}
+	}
+}
+
+var badLuckTimer = function() {
+	this.base = new rw.rule(true);
+	this.rule = function() {
+		if (badLuckCountdown>0) {
+			badLuckCountdown--;
+		} else {
+			badLuck = false;
+		}
+	}
+}
+
+var pickTile = function() {
+	var xTile = Math.round(15*Math.random());
+	var yTile = Math.round(15*Math.random());
+	if ((xTile>heroXTile-2)&&(xTile<heroXTile+2)) {
+		if ((yTile>heroYTile-2)&&(yTile<heroYTile+2)) {
+			var newTiles = pickTile();
+			return newTiles;
+		}
+		else {
+			return [xTile, yTile];
+		}
+	} else {
+		return [xTile, yTile];
+	}
+}
+
+var eyeGenerator = function() {
+	var newTiles = pickTile()
+	var xTile = 30*newTiles[0];
+	var yTile = 30*newTiles[1];
+	if (Math.random()>0.5) {
+		if (Math.random()>0.5) {
+			var heading = 'd';
+		} else {
+			var heading = 'u';
+		}
+	} else {
+		if (Math.random()>0.5) {
+			var heading = 'l';
+		} else {
+			var heading = 'r';
+		}
+	}
+	rw.newEnt(new eye(('eye'+eyeCounter++), heading))
+		.base.display(heading+'1', xTile, yTile, yTile);
 }
 
 var makeEyes = function() {
@@ -129,33 +366,34 @@ var makeEyes = function() {
 			this.counter--;
 		} else {
 			this.counter = 20;
-			var x = Math.round(470*Math.random());
-			var y = Math.round(470*Math.random());
-			if (Math.random()>0.5) {
-				if (Math.random()>0.5) {
-					var heading = 'd';
-				} else {
-					var heading = 'u';
-				}
-			} else {
-				if (Math.random()>0.5) {
-					var heading = 'l';
-				} else {
-					var heading = 'r';
-				}
-			}
-			rw.newEnt(new eye(('eye'+eyeCounter++), heading))
-				.base.display(heading+'1', x, y, y);
+			eyeGenerator();
 		}
 	}
 }
 
+var dropItem = function() {
+	this.base = new rw.rule(true);
+	this.rule = function() {
+		if (Math.random()>0.995) {
+			var newTiles = pickTile()
+			var xPos = 30*newTiles[0];
+			var yPos = 30*newTiles[1];
+			rw.newEnt(new item())
+				.base.display('question', xPos, yPos, yPos);
+		}
+	}
+}
 
 var startGame = function() {
-	rw.init(500,500)
+	rw.init(480,480).tilesOn(30,30)
 	.newRule('makeEyes', new makeEyes())
+	.newRule('fatima', new fatimaTimer())
+	.newRule('blindeye', new blindEyeTimer())
+	.newRule('slow', new slowTimer())
+	.newRule('badluck', new badLuckTimer())
+	.newRule('dropItem', new dropItem())
 	.newEnt(new hero('hero'))
-		.base.display('d1',250,250,250)
+		.base.display('d1',240,240,240)
 		.end()
 	.start();
 }
