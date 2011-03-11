e5629e5df19e966b02c3725479b2e7cd856a18c5
Caz <vonkow@vonkow.(none)>
2011-03-10 20:53:52 -0500

    V0.75

--- builddoc.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-java -jar ../jsdoc-toolkit/jsrun.jar ../jsdoc-toolkit/app/run.js -a -t=../jsdoc-toolkit/templates/jsdoc ./ -d=./docs
-
--- compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-touch temp.js
-cat rosewood.js >> temp.js
-cat $1game.js >> temp.js
-java -jar ../closure-compiler/compiler.jar --compilation_level ADVANCED_OPTIMIZATIONS --js temp.js --js_output_file $1compiled.js
-rm temp.js
